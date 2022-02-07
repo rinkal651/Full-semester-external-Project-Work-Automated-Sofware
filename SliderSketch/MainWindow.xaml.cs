8b02f7b25050962ffa64a33d5ac522b1e44d6902
Rinkal Tailor <85353015+rinkal651@users.noreply.github.com>
2022-02-07 17:20:17 +0530

    Add files via upload

---
+++ SliderSketch/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace Text_Grretings
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+
+
+        }
+        void OnBorderSizeChanged(object sender, SizeChangedEventArgs args)
+       {
+ Border border = sender as Border;
+        xSlider.Maximum = args.NewSize.Width - border.Padding.Left 
+ - border.Padding.Right 
+ - polyline.StrokeThickness;
+ ySlider.Maximum = args.NewSize.Height - border.Padding.Top 
+ - border.Padding.Bottom 
+ - polyline.StrokeThickness;
+ }
+    void OnSliderValueChanged(object sender, RangeBaseValueChangedEventArgs args)
+    {
+        polyline.Points.Add(new Point(xSlider.Value, ySlider.Value));
+    }
+
+
+}
+}
