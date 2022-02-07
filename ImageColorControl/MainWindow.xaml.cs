78b5f75a3d8a8a6e9c2abbcc39db064714fd8c9a
Rinkal Tailor <85353015+rinkal651@users.noreply.github.com>
2022-02-07 17:14:58 +0530

    Add files via upload

---
+++ ImageColorControl/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace Text_Grretings
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+          
+
+        }
+        void OnSliderValueChanged(object sender, RangeBaseValueChangedEventArgs args)
+        {
+            byte r = (byte)redSlider.Value;
+            byte g = (byte)greenSlider.Value;
+            byte b = (byte)blueSlider.Value;
+            redValue.Text = r.ToString("X2");
+            greenValue.Text = g.ToString("X2");
+            blueValue.Text = b.ToString("X2");
+            brushResult.Color = Color.FromArgb(255, r, g, b);
+        }
+
+
+    }
+}
