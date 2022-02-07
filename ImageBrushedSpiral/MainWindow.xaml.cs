a115cf3f276c06b8186024f57b0576d46026ba52
Rinkal Tailor <85353015+rinkal651@users.noreply.github.com>
2022-02-07 16:34:03 +0530

    Add files via upload

---
+++ ImageBrushedSpiral/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace Text_Grretings
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+            for (int angle = 0; angle < 3600; angle++)
+            {
+                double radians = Math.PI * angle / 180;
+                double radius = angle / 10;
+                double x = 360 + radius * Math.Sin(radians);
+                double y = 360 + radius * Math.Cos(radians);
+                polyline.Points.Add(new Point(x, y));
+            }
+        }
+
+      
+    }
+}
