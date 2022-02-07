6c7844cbb17cc8cb2fedfeddbc3b7da896d8d96a
Rinkal Tailor <85353015+rinkal651@users.noreply.github.com>
2022-02-07 16:46:56 +0530

    Add files via upload

---
+++ ColorChangeonTapTextBlock/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace Text_Grretings
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        Random rand = new Random();
+        byte[] rgb = new byte[3];
+        public MainWindow()
+        {
+            this.InitializeComponent();
+
+        }
+        void OnTextBlockTapped(object sender, TappedRoutedEventArgs args)
+        {
+            TextBlock txtblk = sender as TextBlock;
+            rand.NextBytes(rgb);
+            Color clr = Color.FromArgb(255, rgb[0], rgb[1], rgb[2]);
+            txtblk.Foreground = new SolidColorBrush(clr);
+        }
+
+    }
+}
