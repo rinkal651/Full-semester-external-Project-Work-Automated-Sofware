2142e06ced344627cac84426b4c09c6ae2e2725b
Rinkal Tailor <85353015+rinkal651@users.noreply.github.com>
2022-02-07 21:29:12 +0530

    Add files via upload

---
+++ Clock Animation/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.UI;
+using Microsoft.UI.Text;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Documents;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace Text_Grretings
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            CompositionTarget.Rendering += OnCompositionTargetRendering;
+        }
+        void OnCompositionTargetRendering(object sender, object args)
+        {
+            DateTime dt = DateTime.Now;
+            rotateSecond.Angle = 6 * (dt.Second + dt.Millisecond / 1000.0);
+            rotateMinute.Angle = 6 * dt.Minute + rotateSecond.Angle / 60;
+            rotateHour.Angle = 30 * (dt.Hour % 12) + rotateMinute.Angle / 12;
+        }
+    }
+}
+
+    
