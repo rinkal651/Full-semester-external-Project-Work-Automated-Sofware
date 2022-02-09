75b68098b3e49ab5fa7cbd93d0309a991a408ce2
Rinkal Tailor <85353015+rinkal651@users.noreply.github.com>
2022-02-09 23:21:59 +0530

    Add files via upload

---
+++ ActionsOnNavigationWithBinding/MainWindow.xaml.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace Report2_Project
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    ///   
+   class Category
+    {
+        public String Name { get; set; }
+    public String CategoryIcon { get; set; }
+    public ObservableCollection<Category> Children { get; set; }
+}
+public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+
+    ObservableCollection<Category> Categories = new ObservableCollection<Category>()
+    {
+        new Category(){
+            Name = "Menu item 1",
+            CategoryIcon = "Icon",
+            Children = new ObservableCollection<Category>() {
+                new Category(){
+                    Name = "Menu item 2",
+                    CategoryIcon = "Icon",
+                    Children = new ObservableCollection<Category>() {
+                        new Category() {
+                            Name  = "Menu item 3",
+                            CategoryIcon = "Icon",
+                            Children = new ObservableCollection<Category>() {
+                                new Category() { Name  = "Menu item 4", CategoryIcon = "Icon" },
+                                new Category() { Name  = "Menu item 5", CategoryIcon = "Icon" }
+                            }
+                        }
+                    }
+                }
+            }
+        },
+        new Category(){
+            Name = "Menu item 6",
+            CategoryIcon = "Icon",
+            Children = new ObservableCollection<Category>() {
+                new Category(){
+                    Name = "Menu item 7",
+                    CategoryIcon = "Icon",
+                    Children = new ObservableCollection<Category>() {
+                        new Category() { Name  = "Menu item 8", CategoryIcon = "Icon" },
+                        new Category() { Name  = "Menu item 9", CategoryIcon = "Icon" }
+                    }
+                }
+            }
+        },
+        new Category(){ Name = "Menu item 10", CategoryIcon = "Icon" }
+    };
+
+        private void OnItemInvoked(object sender, NavigationViewItemInvokedEventArgs e)
+        {
+            var clickedItem = e.InvokedItem;
+            var clickedItemContainer = e.InvokedItemContainer;
+        }
+        private void OnItemExpanding(object sender, NavigationViewItemExpandingEventArgs e)
+        {
+            var nvib = e.ExpandingItemContainer;
+            var name = "Last expanding: " + nvib.Content.ToString();
+            ExpandingItemLabel.Text = name;
+        }
+        private void OnItemCollapsed(object sender, NavigationViewItemCollapsedEventArgs e)
+        {
+            var nvib = e.CollapsedItemContainer;
+            var name = "Last collapsed: " + nvib.Content;
+            CollapsedItemLabel.Text = name;
+        }
+    }
+    
+}
