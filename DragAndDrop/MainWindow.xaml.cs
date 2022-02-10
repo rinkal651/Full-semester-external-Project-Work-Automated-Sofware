14362237488e53041fd90c35f65c56357890297f
Rinkal Tailor <85353015+rinkal651@users.noreply.github.com>
2022-02-10 07:41:41 +0530

    Add files via upload

---
+++ DragAndDrop/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Media.Imaging;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.DataTransfer;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace Report2_Project
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    ///   
+    
+
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+           
+                  }
+        private async void Grid_Drop(object sender, DragEventArgs e)
+        {
+            if (e.DataView.Contains(StandardDataFormats.StorageItems))
+            {
+                var items = await e.DataView.GetStorageItemsAsync();
+                if (items.Count > 0)
+                {
+                    var storageFile = items[0] as StorageFile;
+                    var bitmapImage = new BitmapImage();
+                    bitmapImage.SetSource(await storageFile.OpenAsync(FileAccessMode.Read));
+                    // Set the image on the main page to the dropped image
+                    Image.Source = bitmapImage;
+                }
+            }
+        }
+        private void Grid_DragOver(object sender, DragEventArgs e)
+        {
+            e.AcceptedOperation = DataPackageOperation.Copy;
+        }
+
+        private void Grid_DragOverCustomized(object sender, DragEventArgs e)
+        {
+            e.AcceptedOperation = DataPackageOperation.Copy;
+            e.DragUIOverride.Caption = "Custom text here"; // Sets custom UI text
+                                                           // Sets a custom glyph
+            e.DragUIOverride.SetContentFromBitmapImage(
+                new BitmapImage(
+                    new Uri("F://semester-7//kitl reports//Graphical Greetings//Graphical Greetings//bvm.jpg", UriKind.RelativeOrAbsolute)));
+            e.DragUIOverride.IsCaptionVisible = true; // Sets if the caption is visible
+            e.DragUIOverride.IsContentVisible = true; // Sets if the dragged content is visible
+            e.DragUIOverride.IsGlyphVisible = true; // Sets if the glyph is visibile
+        }
+
+    }
+
+
+    }
+    
+
