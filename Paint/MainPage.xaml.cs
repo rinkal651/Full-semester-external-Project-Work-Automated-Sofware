d3c392f037705f04f6c42f9364ba71f45044e497
Rinkal Tailor <85353015+rinkal651@users.noreply.github.com>
2022-02-10 21:17:30 +0530

    Add files via upload

---
+++ Paint/MainPage.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Shapes;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace App_Report2
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        Dictionary<uint, Polyline> pointerDictionary = new Dictionary<uint, Polyline>();
+        Random rand = new Random();
+        byte[] rgb = new byte[3];
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnPointerPressed(PointerRoutedEventArgs args)
+        {
+            // Get information from event arguments
+            uint id = args.Pointer.PointerId;
+            Point point = args.GetCurrentPoint(this).Position;
+      // Create random color
+ rand.NextBytes(rgb);
+            Color color = Color.FromArgb(255, rgb[0], rgb[1], rgb[2]);
+            // Create Polyline
+            Polyline polyline = new Polyline
+            {
+                Stroke = new SolidColorBrush(color),
+                StrokeThickness = 24,
+            };
+            polyline.Points.Add(point);
+            // Add to Grid
+            contentGrid.Children.Add(polyline);
+            // Add to dictionary
+            pointerDictionary.Add(id, polyline);
+            base.OnPointerPressed(args);
+        }
+        protected override void OnPointerMoved(PointerRoutedEventArgs args)
+        {
+            // Get information from event arguments
+            uint id = args.Pointer.PointerId;
+            Point point = args.GetCurrentPoint(this).Position;
+            // If ID is in dictionary, add the point to the Polyline
+            if (pointerDictionary.ContainsKey(id))
+                pointerDictionary[id].Points.Add(point);
+            base.OnPointerMoved(args);
+        }
+        protected override void OnPointerReleased(PointerRoutedEventArgs args)
+        {
+            // Get information from event arguments
+            uint id = args.Pointer.PointerId;
+            // If ID is in dictionary, remove it
+            if (pointerDictionary.ContainsKey(id))
+                pointerDictionary.Remove(id);
+            base.OnPointerReleased(args);
+        }
+    }
+}
+
