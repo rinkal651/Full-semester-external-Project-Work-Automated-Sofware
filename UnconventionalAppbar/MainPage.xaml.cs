3443a144e9291ad3e80f2c6dc9b9cf6dee3b4b42
Rinkal Tailor <85353015+rinkal651@users.noreply.github.com>
2022-02-10 20:27:09 +0530

    Add files via upload

---
+++ UnconventionalAppbar/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Microsoft.UI;
+using Windows.UI.Core;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.Storage.Pickers;
+using Windows.Storage;
+using Windows.Storage.Streams;
+using System.Threading.Tasks;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Report2_Program
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+
+    public sealed partial class MainPage : Page
+    {
+      
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+        void OnAppBarButtonClick(object sender, RoutedEventArgs args)
+        {
+       
+            textBlock.Foreground = (sender as Button).Foreground;
+            topAppBar.IsOpen = false;
+            bottomAppBar.IsOpen = false;
+        }
+        }
+    }
